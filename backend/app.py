4546cf6e3d6bee745493f8561a81bddf6c059d5b
uggi121 <sudharshanmadhavan@outlook.com>
2020-10-09 18:51:53 +0530

    Create registration endpoints

--- backend/app.py
+++ backend/app.py
@@ -203,6 +203,12 @@ def new_student():
     db.session.commit()
     return "OK", 200
 
+@app.route("/students/register", methods=['POST'])
+def register_student():
+    data = request.form.to_dict()
+    db.session.merge(Student(**data))
+    db.session.commit()
+    return "OK", 200
 
 @app.route("/volunteers/all", methods=['GET'])
 def get_all_volunteers():
@@ -222,6 +228,12 @@ def new_volunteer():
     db.session.commit()
     return "OK", 200
 
+@app.route("/volunteers/register", methods=['POST'])
+def register_volunteer():
+    data = request.form.to_dict()
+    db.session.merge(Volunteer(**data))
+    db.session.commit()
+    return "OK", 200
 
 @app.route("/programs/all", methods=['GET'])
 def get_all_programs():
