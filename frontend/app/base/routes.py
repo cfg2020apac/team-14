fa51a47f70e4400d9e21cad082df68835dd53600
zy tan <zarn@wright.com.sg>
2020-10-09 21:51:56 +0800

    added program view

--- frontend/app/base/routes.py
+++ frontend/app/base/routes.py
@@ -141,3 +141,13 @@ def add_student():
 
 
     return render_template('students_view.html')
+
+
+@blueprint.route('/programs_view')
+def return_programs():
+    response = requests.get("http://danieltan.org:8080/programs/all")
+    data = response.json()
+    for k in data:
+        data = data[k]
+
+    return render_template('programs_view.html', data=data)
